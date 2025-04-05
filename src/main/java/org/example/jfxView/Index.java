3be45c2a57afbf20cd839d1fdfa7018d70c8f6d3
Cal <d00269431@student.dkit.ie>
2025-04-05 14:25:53 +0100

    fixed javafx to operate successfully

---
+++ src/main/java/org/example/jfxView/Index.java
@@ -0,0 +1,26 @@
+package org.example.jfxView;
+
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Index extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/index.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setScene(scene);
+        stage.setTitle("Hello!");
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);  // Pass args here
+    }
+}
